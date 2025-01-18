1a602255cd9756056a5ee8daab0df3602f26e8cb
JusGoWithTheFLo <107014024+JusGoWithTheFLo@users.noreply.github.com>
2025-01-17 22:50:53 -0500

    Verifying SSH connection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar(){
             <ul className="navbar-list">
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
-                <li><a href="">Contacts</a></li>
+                <li><a href="">Contact</a></li>
             </ul>
         </nav>
     )
